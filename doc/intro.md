746eb7ecf7d4ca0acdb12748beef4234ea155735
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-01 19:10:01 +0200

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lifescope-stats
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lifescope_stats/core_test.clj
@@ -0,0 +1,7 @@
+(ns lifescope-stats.core-test
+  (:require [clojure.test :refer :all]
+            [lifescope-stats.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
