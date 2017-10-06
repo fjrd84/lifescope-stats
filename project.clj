d42896f1852e0937c1a77bf9cf39879c895c0fba
F. Javier R. Donado <fjrd84@gmail.com>
2017-10-06 22:19:11 +0200

    Add ring.

--- project.clj
+++ project.clj
@@ -6,9 +6,22 @@
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  ; The lein-ring plugin allows us to easily start a development web server
+  ; with "lein ring server". It also allows us to package up our application
+  ; as a standalone .jar or as a .war for deployment to a servlet contianer
+  ; (I know... SO 2005).
+  :plugins [[lein-ring "0.8.10"]]
+  
+  ; See https://github.com/weavejester/lein-ring#web-server-options for the
+  ; various options available for the lein-ring plugin
+  :ring {:handler restful-clojure.handler/app
+         :nrepl {:start? true
+                 :port 9998}}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.2.1"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [ring-mock "0.1.5"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [compojure "1.6.0"]]
 
