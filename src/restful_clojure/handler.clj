fe4cf72f82a5070bfe05e3db5edbe1565c628ee8
F. Javier R. Donado <fjrd84@gmail.com>
2017-10-08 21:43:44 +0200

    Add logger and not-route-found handler.

--- src/restful_clojure/handler.clj
+++ src/restful_clojure/handler.clj
@@ -1,6 +1,9 @@
 (ns restful-clojure.handler
-  (:use compojure.core)
-  (:require [compojure.handler :as handler]))
+  (:use compojure.core
+        ring.middleware.json)
+  (:require [compojure.handler :as handler] 
+            [ring.util.response :refer [response]]
+            [compojure.route :as route]))
 
 (defn- str-to [num]
   (apply str (interpose ", " (range 1 (inc num)))))
@@ -8,6 +11,23 @@
 (defn- str-from [num]
   (apply str (interpose ", " (reverse (range 1 (inc num))))))
 
-(defroutes app
+(defroutes app-routes
   (GET "/count-up/:to" [to] (str-to (Integer. to)))
-  (GET "/count-down/:from" [from] (str-from (Integer. from))))
+  (GET "/count-down/:from" [from] (str-from (Integer. from)))
+  (route/not-found
+   (response {:message "Page not found"}))
+  )
+
+
+; Middleware: logger
+(defn wrap-log-request [handler]
+  (fn [req]
+    [println req]
+    (handler req)))
+
+(def app
+  ( -> app-routes
+       wrap-log-request
+       wrap-json-response
+       wrap-json-body
+       ))
