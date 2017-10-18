68e425c537b8613b974804c15e8be8f2b4788c29
F. Javier R. Donado <fjrd84@gmail.com>
2017-10-18 21:22:32 +0200

    Elastic search query example.

--- src/lifescope_stats/rest.clj
+++ src/lifescope_stats/rest.clj
@@ -4,14 +4,25 @@
   (:require [compojure.handler :as handler] 
             [ring.util.response :refer [response]]
             [clojurewerkz.elastisch.rest :as esr]
+            [clojurewerkz.elastisch.rest.document :as esd]
+            [clojurewerkz.elastisch.query :as q]
+            [clojurewerkz.elastisch.rest.response :as esrsp]
+            [clojure.pprint :as pp]
             [compojure.route :as route]))
 
 ; Read the configuration file
 (def config (read-string (slurp "config.clj")))
 
-(let [conn (esr/connect (:url (:elastic config))
-                        {:basic-auth [(:user (:elastic config))
-                                      (:password (:elastic config))]})])
+(def conn (esr/connect (:url (:elastic config))
+                       {:basic-auth [(:user (:elastic config))
+                                     (:password (:elastic config))]}))
+
+(defn testme [] (let [res  (esd/search conn "analysis" (q/term :source "twitter"))
+                      n    (esrsp/total-hits res)
+                      hits (esrsp/hits-from res)]
+                  (println (format "Total hits: %d" n))
+                  (println (format "Test:" res))
+                  (pp/pprint hits)))
 
 (defn- str-to [num]
   (apply str (interpose ", " (range 1 (inc num)))))
@@ -21,11 +32,13 @@
 
 (defroutes app-routes
   (GET "/" [] (response {:message "Lifescope Stats API"}))
-  (GET "/search/:word" [to] (str-to (Integer. to)))
+  (GET "/search/:word" [word]
+       (response (esd/search conn "analysis" :query
+                             (q/term :source word))))
   (route/not-found
    (response {:message "Page not found"})))
 
-; Middleware: logger
+                                        ; Middleware: logger
 (defn wrap-log-request [handler]
   (fn [req]
     (println req)
