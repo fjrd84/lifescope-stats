9cffa3bbeea740dbf38d413e41dacbf0aba0d88e
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-01 12:35:34 +0100

    Read configuration file.

--- src/lifescope_stats/core.clj
+++ src/lifescope_stats/core.clj
@@ -2,5 +2,7 @@
   (:require [lifescope-stats.rest :refer :all]
             [ring.adapter.jetty :as jetty]))
 
+(def config (read-string (slurp "config.clj")))
+
 (defn -main []
   (jetty/run-jetty app {:port (:port config)}))
