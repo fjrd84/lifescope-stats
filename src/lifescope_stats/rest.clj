21e68546206e0bc51882e04fafe15734daafb19b
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-01 12:35:13 +0100

    Remove unused dependencies.

--- src/lifescope_stats/rest.clj
+++ src/lifescope_stats/rest.clj
@@ -2,33 +2,9 @@
   (:use [compojure.core]
         [ring.middleware.json])
   (:require [compojure.handler :as handler]
+            [lifescope-stats.stats :refer :all]
             [ring.util.response :refer [response]]
-            [clojurewerkz.elastisch.rest :as esr]
-            [clojurewerkz.elastisch.rest.document :as esd]
-            [clojurewerkz.elastisch.query :as q]
-            [clojurewerkz.elastisch.rest.response :as esrsp]
-            [clojure.pprint :as pp]
             [compojure.route :as route]))
-
-; Read the configuration file
-(def config (read-string (slurp "config.clj")))
-
-(def conn (esr/connect (:url (:elastic config))
-                       {:basic-auth [(:user (:elastic config))
-                                     (:password (:elastic config))]}))
-
-
-(defn wildcard-search [search-word]
-  (let [res (esd/search conn
-                        "analysis"
-                        ""
-                        :query
-                        (q/wildcard :query (str search-word "*")))
-        hits (esrsp/hits-from res)
-        n (esrsp/total-hits res)]
-    {:results hits :count n}
-    ))
-
 
 (defroutes app-routes
   (GET "/" [] (response {:message "Lifescope Stats API"}))
