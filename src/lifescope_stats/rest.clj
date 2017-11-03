eb716129e837c3dd44a1ec6be9ef349f758f13b5
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-03 23:28:19 +0100

    Add problems endpoint and total count of messages on root.

--- src/lifescope_stats/rest.clj
+++ src/lifescope_stats/rest.clj
@@ -7,9 +7,11 @@
             [compojure.route :as route]))
 
 (defroutes app-routes
-  (GET "/" [] (response {:message "Lifescope Stats API"}))
+  (GET "/" [] (response {:message "Lifescope Stats API" :messages-count (:count (total-count))}))
   (GET "/query-count/" [word]
        (response (all-queries-count)))
+  (GET "/problems/" []
+       (response (find-all-problems)))
   (GET "/solutions/:problem" [problem]
        (response (find-solutions-to-problem problem)))
   (GET "/search/:word" [word]
